22cebbbe28b41e23bfa6879efea658ab7b75585f
LiuKun-JupyterNotebook <liukunup@163.com>
2021-08-28 16:33:07 +0000

    新增远端资源下载脚本

---
+++ resources/download.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# author : Liu Kun
+# date   : 2021-08-27 23:46:55
+
+# lena.jpg
+wget https://github.com/opencv/opencv/blob/master/samples/data/lena.jpg
+# chessboard.png
+wget https://github.com/opencv/opencv/blob/master/samples/data/chessboard.png

aee4c35a358d8f0b2f3b0ff3ff2c4f561339ceea
LiuKun-JupyterNotebook <liukunup@163.com>
2021-08-29 16:30:46 +0000

    补提交kit_minio.py的更新

--- utils/kit_minio.py
+++ utils/kit_minio.py
@@ -1,22 +1,36 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 # author : Liu Kun
-# date   : 2021-08-27 23:46:55
+# date   : 2021-08-29 23:31:55
+
+import os
+from minio import Minio
+
 
 class MinIO(object):
     
-    def __init__(self):
+    __client = None
+    
+    def __init__(self, host, port, access_key, secret_key):
+        self.__client = Minio(f"{host}:{port}", access_key=access_key, secret_key=secret_key, secure=False)
         pass
     
-    def xxx(self):
-        print("Hello")
+    def upload(self, fr, to, bucket):
+        if not os.access(fr, os.F_OK):
+            return None
+        if not self.__client.bucket_exists(bucket):
+            self.__client.make_bucket(bucket)
+        result = self.__client.fput_object(bucket, to, fr)
+        print("created {0} object; etag: {1}, version-id: {2}".format(
+            result.object_name, result.etag, result.version_id,),)
         pass
     
-    def yyy(self):
-        self.xxx()
+    def download(self, fr, to, bucket):
+        if os.access(to, os.F_OK):
+            os.remove(to)
+        self.__client.fget_object(bucket, fr, to)
         pass
-
-if __name__ == '__main__':
-    inst = MinIO()
-    inst.yyy()
-    pass
+    
+    def remove(self, target, bucket):
+        self.__client.remove_object(bucket, target)
+        pass

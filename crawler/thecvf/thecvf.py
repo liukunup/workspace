a44f57dd42c9c16b381b79827cbc39d5a017b382
Liu Kun <liukunup@163.com>
2022-05-14 17:36:22 +0800

    论文爬取支持ECCV

--- crawler/thecvf/thecvf.py
+++ crawler/thecvf/thecvf.py
@@ -65,6 +65,64 @@ def request_paper_list(url, debug=False):
     return papers
 
 
+def request_eccv_paper_list(url="https://www.ecva.net/papers.php", year=None, debug=False):
+    """
+    ECCV
+    :param url:   目标链接
+    :param year:  年份标识
+    :param debug: 调试信息打印开关[默认关闭]
+    :return: 论文信息列表
+    """
+    # 参数检查
+    if year is None or not isinstance(year, str) or len(year) <= 0:
+        return list()
+    # 获取标识 ECCV2018 -> 2018
+    flag = year.upper().replace("ECCV", "")
+    # 请求页面
+    base_url = f"https://www.ecva.net"
+    response = requests.get(url)
+    # 使用BeautifulSoup加载页面
+    soup = BeautifulSoup(response.content, features="lxml")
+    # 按标签查找
+    div_list = soup.find_all("div", attrs={"class": "accordion-content"})
+    # 用于存放论文信息
+    papers = list()
+    for div in div_list:
+        dt_list = div.find_all("dt")
+        dd_list = div.find_all("dd")
+        # 按奇偶处理dd标签
+        dd_odd_list = dd_list[::2]
+        dd_even_list = dd_list[1::2]
+        # 逐行处理
+        for item in zip(dt_list, dd_odd_list, dd_even_list):
+            # Line 1
+            line_1 = item[0]
+            tag_a_1 = line_1.find("a")
+            # Line 2
+            line_2 = item[1]
+            # Line 3
+            line_3 = item[2]
+            tag_a_2 = line_3.find("a")
+            paper_json = {
+                "web_url": base_url + tag_a_1["href"],
+                "title": tag_a_1.text.strip(),
+                "author": line_2.text.strip().replace("\n", "").replace("\r", ""),
+                "pdf_url": base_url + tag_a_2["href"],
+            }
+            # 修复链接中缺少/链接符的问题
+            if not str(tag_a_1["href"]).startswith("/"):
+                paper_json["web_url"] = base_url + "/" + tag_a_1["href"]
+            if not str(tag_a_2["href"]).startswith("/"):
+                paper_json["pdf_url"] = base_url + "/" + tag_a_2["href"]
+            if debug:
+                print("-" * 100)
+                print(json.dumps(paper_json, sort_keys=True, indent=4, separators=(',', ': ')))
+            if paper_json["web_url"].find(flag) != -1:
+                papers.append(paper_json)
+    print("共爬取到论文信息%d条" % len(papers))
+    return papers
+
+
 def export_jsonl(papers, filename):
     """
     导出为jsonl格式
@@ -122,28 +180,54 @@ class CLI(cmd.Cmd):
         with open("menu.json", "r") as f:
             self.__menu_list = json.load(f)
 
-    def do_crawler(self, _):
+    def do_crawler(self, arg):
         """
         爬取论文信息
-        :param _: 无需输入
+        :param arg: 即将爬取的目录+年份(如CVPR2021)
         :return: 无
         """
-        for target in self.__menu_list:
-            # 打印待爬取信息
-            print(target)
-            sub_path = target["path"]
-            # 如文件夹不存在则创建
-            if not os.path.exists(sub_path):
-                os.makedirs(sub_path)
-            logfile = target["logfile"]
-            target_url_list = target["links"]
-            for target_url in target_url_list:
-                # 爬取论文链接
-                paper_list = request_paper_list(target_url)
-                # 记录论文信息到日志文件
-                fn = os.path.join(sub_path, logfile)
-                export_jsonl(paper_list, fn)
-                export_url(paper_list, fn.replace("jsonl", "txt"))
+        if arg is not None and len(arg) > 0:
+            for target in self.__menu_list:
+                # 打印待爬取信息
+                sub_path = target["path"]
+                if sub_path != arg:
+                    continue
+                print(target)
+                # 如文件夹不存在则创建
+                if not os.path.exists(sub_path):
+                    os.makedirs(sub_path)
+                logfile = target["logfile"]
+                target_url_list = target["links"]
+                for target_url in target_url_list:
+                    # 爬取论文链接
+                    if sub_path.startswith("ECCV"):
+                        paper_list = request_eccv_paper_list(target_url, year=sub_path)
+                    else:
+                        paper_list = request_paper_list(target_url)
+                    # 记录论文信息到日志文件
+                    fn = os.path.join(sub_path, logfile)
+                    export_jsonl(paper_list, fn)
+                    export_url(paper_list, fn.replace("jsonl", "txt"))
+        else:
+            for target in self.__menu_list:
+                # 打印待爬取信息
+                print(target)
+                sub_path = target["path"]
+                # 如文件夹不存在则创建
+                if not os.path.exists(sub_path):
+                    os.makedirs(sub_path)
+                logfile = target["logfile"]
+                target_url_list = target["links"]
+                for target_url in target_url_list:
+                    # 爬取论文链接
+                    if sub_path.startswith("ECCV"):
+                        paper_list = request_eccv_paper_list(target_url, year=sub_path)
+                    else:
+                        paper_list = request_paper_list(target_url)
+                    # 记录论文信息到日志文件
+                    fn = os.path.join(sub_path, logfile)
+                    export_jsonl(paper_list, fn)
+                    export_url(paper_list, fn.replace("jsonl", "txt"))
 
     def do_download(self, arg):
         """

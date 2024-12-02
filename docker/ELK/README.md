ccece05fb9dd6c78f88b9f0bfdc076d28934ee58
刘坤 <liukunup@163.com>
2024-12-02 03:40:53 +0000

    清理一下

--- docker/ELK/README.md
+++
@@ -1,9 +0,0 @@
-# Elastic Stack (ELK)
-
-## ELK简介
-
-- ElasticSearch 分布式搜索引擎
-- Logstash 数据收集处理引擎
-- Kibana 可视化平台
-
-感觉做的不完美, 后续再优化吧
--- resources/download.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# author : Liu Kun
-# date   : 2021-08-27 23:46:55
-
-# lena.jpg
-wget https://github.com/opencv/opencv/blob/master/samples/data/lena.jpg
-# chessboard.png
-wget https://github.com/opencv/opencv/blob/master/samples/data/chessboard.png
